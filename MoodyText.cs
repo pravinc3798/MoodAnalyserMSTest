4e339d7525f86f34f6918736d1a489c368f6faf7
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-01 19:50:29 +0530

    Added MoodyText class

---
+++ MoodyText.cs
@@ -0,0 +1,42 @@
+﻿namespace MoodAnalyserMSTest
+{
+    public class MoodyText
+    {
+        public string Text;
+
+        public MoodyText()
+        {
+            // default blank constructor
+        }
+
+        public MoodyText(string text)
+        {
+            Text = text;
+        }
+
+        public string AnalyseMood(string text)
+        {
+            try
+            {
+                var check = (text.ToLower().Contains("sad")) ? "SAD" : "HAPPY";
+                return check;
+            }
+            catch (NullReferenceException)
+            {
+                return "HAPPY";
+            }
+        }
+
+        public string AnalyseMood()
+        {
+            if (Text == null)
+                throw new MoodyTextExceptions(MoodyTextExceptions.CustomExceptions.NullMood, "Null Mood");
+
+            if (Text.Length == 0)
+                throw new MoodyTextExceptions(MoodyTextExceptions.CustomExceptions.EmptyMood, "Empty Mood");
+
+            var check = (Text.ToLower().Contains("sad")) ? "SAD" : "HAPPY";
+            return check;
+        }
+    }
+}
