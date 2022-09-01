82986eff427ba29527230bb6b5c9abc37e748457
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-01 19:50:35 +0530

    Added MoodyTextExceptions class

---
+++ MoodyTextExceptions.cs
@@ -0,0 +1,18 @@
+﻿namespace MoodAnalyserMSTest
+{
+    public class MoodyTextExceptions : Exception
+    {
+        public enum CustomExceptions
+        {
+            EmptyMood,
+            NullMood
+        }
+
+        public CustomExceptions Ex;
+
+        public MoodyTextExceptions (CustomExceptions ex, string message) : base(message)
+        {
+            Ex = ex;
+        }
+    }
+}
